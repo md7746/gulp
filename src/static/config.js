d287c53f3c2d92867b335c15f277fa5091336812
dm2002 <774681508@qq.com>
2018-05-23 22:26:16 +0800

    gulp构建修改3

--- src/static/config.js
+++ src/static/config.js
@@ -1,11 +1,12 @@
 require.config({
     baseUrl: 'http://localhost:3000/js',
     paths : {
-        "jquery" : ["https://cdn.bootcss.com/jquery/1.9.0/jquery.min",'jquery.min'],
-        'domReady':['https://cdn.bootcss.com/require-domReady/2.0.1/domReady','domReady'],
+        "jquery" : ["https://cdn.bootcss.com/jquery/1.9.0/jquery.min","jquery.min"],
+        "doT":["https://cdn.bootcss.com/dot/2.0.0-beta.0/doT.min","doT.min"],
         'slide':'superslide' 
     },
     shim:{
-        'slide':['jquery']
+        'slide':['jquery'],
+        'doT':{ exports : 'doT'}
     }
 });
